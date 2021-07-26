720b85be9c334315fe13e1df3065182cf69fd667
John Nkereuwem <john.nkereuwem@outlook.com>
2021-07-26 16:59:41 +0100

    Added login form

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 let callMode = document.getElementsByClassName('exit')[0]
+let close = document.getElementsByClassName('logout')[0]
 let btn = document.getElementById('btn').addEventListener
-('click', buttonClick );
+('click', buttonClick);
+let btnLogin = document.getElementById('login').addEventListener
+('click', loginAcc );
 let modal = document.getElementsByClassName('modal-wrapper')[0];
+let login = document.getElementsByClassName('login-wrapper')[0];
 
-
-function buttonClick(){
-      modal.classList.add('active');
-  }
-
-// btn.addEventListener('click', function(){
-//     modal.classList.add('modal-active');
-// })
+// Exit Create account
 
 callMode.addEventListener('click', ()=>{
     modal.classList.remove('active');
 })
 
-console.log(modal)
+close.addEventListener('click', ()=>{
+    login.classList.remove('active');
+})
+
+// Create account
+function buttonClick(){
+      modal.classList.add('active');
+  }
+
+
+function loginAcc(){
+      login.classList.add('active');
+  }
